69ead1b4584962c91100236c68d3ae9f953a9f38
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-18 15:18:38 +0300

    btn addEventListener

--- JS-Vowels In A String/app.js
+++ JS-Vowels In A String/app.js
@@ -2,3 +2,7 @@
 const textarea = document.querySelector("#vowelstextarea");
 const message = document.querySelector(".messagearea");
 const btn = document.querySelector(".button");
+
+btn.addEventListener("click", () => {});
+
+const vowels = (sentence) => {};
